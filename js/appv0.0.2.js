c74bf8f6a57e69893c51c5cd48d8c50f707c61ac
Luke Davis <luke@peopleofthebit.com>
2014-04-01 21:30:33 -0400

    cleaned up the UI a bit by adding an obvious way to select singles double or triples to count. should probably still add a free-form count option, but i've only got so much time. also added the js files locally to the sub-directory (it was just davis and jquery 1.10)

--- js/appv0.0.2.js
+++ js/appv0.0.2.js
@@ -21,21 +21,35 @@ app.c.listeners=function(){
 
 $("input#count").click(function(){
 	var input=$("#input").val();
-	input=app.c.ziph(input);
+	var count=$("input[name=count]:checked").val();
+	input=app.c.ziph(input,parseInt(count),true);
 	var sortable=[];
 	for (key in input){sortable.push([key,input[key].count]);}
 	sortable.sort(function(a,b){return b[1]-a[1];});
 	var d="";
-	for (var i=0;i<sortable.length;i++){d+=sortable[i][0]+" "+sortable[i][1]+"<br>";}
+	d+="<table>";
+	for (var i=0;i<sortable.length;i++){
+		var rowClass="odd";
+		if (i%2==0){rowClass="even";}
+		d+="<tr class='"+rowClass+"'><td>"+sortable[i][1]+"</td><td>"+sortable[i][0]+"</td></tr>";
+	}
+	d+="</table>";
 	$("#output").html(d);
 });
 
 };
 
-app.c.ziph=function(sample,n){
+app.c.ziph=function(sample,n,spaces){
+	var spaces=spaces||false;
 	var n=n||7;
 	var lex={};
-	sample=sample.split("");
+	var sample=sample.toLowerCase();
+	if (spaces==true){
+		sample=sample.split(" ");
+	}
+	else{
+		sample=sample.split("");
+	}
 
 	for (var i=0;i<=(sample.length-n);i++){
 		//then populate the lexicon
@@ -43,7 +57,12 @@ app.c.ziph=function(sample,n){
 			for (var j=0;j<n;j++){
 				compilation.push(sample[i+j])
 			}
-			compilation=compilation.join("");
+			if (spaces==true){
+				compilation=compilation.join(" ");
+			}
+			else{
+				compilation=compilation.join("");
+			}
 
 		if (!lex[compilation]){
 			lex[compilation]={};
@@ -62,8 +81,11 @@ app.c.ziph=function(sample,n){
 app.v.init=function(){
 	app.v.style();
 	var d="";
-	d+="<h1>ziph</h1>";
-	d+="<table width='100%' ><tr><td>";
+	d+="<table width='100%' id='layout'><tr><td colspan='5' id='area-right'>";
+		d+="<h1>ziph</h1>";
+		d+="<input type='radio' value='1' name='count' checked><label>count by singletons</label></br>";
+		d+="<input type='radio' value='2' name='count'><label>count by doubles</label><br/>";
+		d+="<input type='radio' value='3' name='count'><label>count by triples</label><br>";
 		d+="<textarea rows='10' cols='5' id='input' autofocus></textarea>";
 		d+="<input type='button' value='count' id='count'></input>";
 	d+="</td><td id='output'>";
@@ -87,6 +109,9 @@ app.v.style=function(){
 		"font-size":"1.5em",
 		"margin":"30px"
 	});
+	davis.style("h1",{
+		"text-align":"left"
+	});
 	davis.style("input[type=button]",{
 		"width":"100%",
 		"font-size":"2em"
@@ -94,16 +119,35 @@ app.v.style=function(){
 	davis.style("textarea",{
 		"width":"100%",
 		"font-size":"1.5em",
-		"margin":"0"
+		"margin":"0",
+		"margin-top":"30px"
 	});
 	davis.style("table",{
-		"width":"100%",
+		"width":"100%"
+	});
+	davis.style("table#layout",{
+		"height":"100%",
 		"table-layout":"fixed"
 	});
 	davis.style("td",{
 		"padding":"20px",
 		"margin":"30px",
 		"vertical-align":"top",
-		"text-align":"center"
+		"text-align":"left"
+	});
+	davis.style("td#output",{
+		"background":"#f37",
+		"text-align":"left",
+		"padding":"0"
+	});
+	davis.style("td#output td",{
+		"padding":"3px",
+		"margin":"0",
+	});
+	davis.style("tr.odd td",{
+		"background":"#f67"
+	});
+	davis.style("td#output td:hover",{
+		"background":"#555"
 	});
 };
